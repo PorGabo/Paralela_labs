c8a377ec1f58a01929f2cad3b975dc1d8b03bdb2
PorGabo <90857595+PorGabo@users.noreply.github.com>
2024-08-28 15:19:18 -0500

    Update run.sh

--- Comportamiento de Cache/run.sh
+++ Comportamiento de Cache/run.sh
@@ -8,4 +8,7 @@ g++ -std=c++17 comparacion3.cpp -o comparacion3.exe
 ./comparacion3.exe
 
 
-
+g++ -std=c++17 clasica_simple.cpp -o clasica_simple.exe
+./clasica_simple.exe
+g++ -std=c++17 bloques_simple.cpp -o bloques_simple.exe
+./bloques_simple.exe
